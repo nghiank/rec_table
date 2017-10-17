e98cd762368ed78bde99a6779633c43803dbe1ce
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-10-17 07:54:38 -0700

    Split setting to dev & prod

--- web/turk/turk/settings_prod.py
+++ web/turk/turk/settings_prod.py
@@ -4,7 +4,7 @@ from .base import *
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['./templates'],
+        'DIRS': ['web/turk/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -17,13 +17,21 @@ TEMPLATES = [
     },
 ]
 
+WSGI_APPLICATION = 'turk.wsgi.application'
+DEBUG_LOG_DIR = '/var/log/app_logs/django_debug.log'
+# Database
+# brew install postgresm/en/1.11/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'nghia',
-        'USER': 'turk_user',
-        'PASSWORD': 'dev-test-123',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
+
+ALLOWED_HOSTS = [
+    'rec-table-dev.ap-southeast-1.elasticbeanstalk.com',
+]
