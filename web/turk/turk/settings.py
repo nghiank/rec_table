36b68b7ce06e2812f2b6945cccf8ea27daca9234
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-10-16 23:54:10 -0700

    fix build error

--- web/turk/turk/settings.py
+++ web/turk/turk/settings.py
@@ -154,5 +154,5 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_S3_REGION_NAME = 'ap-southeast-1'
 
 ALLOWED_HOSTS = [
-    rec-table-dev.ap-southeast-1.elasticbeanstalk.com,
+    'rec-table-dev.ap-southeast-1.elasticbeanstalk.com',
 ]
