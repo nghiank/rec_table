928c065a5ecbcacbcc5318170f02d72d45a77a72
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-11-16 08:24:15 -0800

    Fix bash

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 TENSORFLOW_PATH = "/opt/python/current/app/tensorflow"
-if [ ! -f  $TENSORFLOW_PATH]; then
+if [ ! -d  $TENSORFLOW_PATH]; then
     mkdir $TENSORFLOW_PATH
 fi
 virtualenv --system-site-packages -p python3 $TENSORFLOW_PATH 
