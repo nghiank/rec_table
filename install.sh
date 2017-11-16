55873a1ba1b3b5ccd972f404db4898407af8bc28
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-11-16 08:35:49 -0800

    Update folder

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ if [ ! -d  $TENSORFLOW_PATH]; then
     mkdir $TENSORFLOW_PATH
 fi
 virtualenv --system-site-packages -p python3 $TENSORFLOW_PATH 
-source /opt/python/current/app/tensorflow/bin/activate
 pip3 install --upgrade tensorflow
+source /opt/python/current/app/tensorflow/bin/activate
 pip3 install Pillow
