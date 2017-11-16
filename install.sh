7bf03a2c9886ebc54546f934fa8ca7278939772a
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-11-16 08:16:40 -0800

    Change tensorflow installation path

--- install.sh
+++ install.sh
@@ -1,9 +1,8 @@
-TENSORFLOW_PATH = "/home/ec2-user/tensorflow"
+TENSORFLOW_PATH = "/opt/python/current/app/tensorflow"
 if [ ! -f  $TENSORFLOW_PATH]; then
     mkdir $TENSORFLOW_PATH
 fi
 virtualenv --system-site-packages -p python3 $TENSORFLOW_PATH 
-source /home/ec2-user/tensorflow/bin/activate
+source /opt/python/current/app/tensorflow/bin/activate
 pip3 install --upgrade tensorflow
 pip3 install Pillow
-
--- web/turk/run_prediction.sh
+++ web/turk/run_prediction.sh
@@ -1,5 +1,5 @@
-source /home/ec2-user/tensorflow/bin/activate
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$DIR/../../tensorflow/bin/activate"
 pushd "$DIR/../../end_to_end/"
 echo "Input:" + "$@" 
 python3 rec_table.py "$@"
