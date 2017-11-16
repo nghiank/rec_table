3fcc57c445e5b08907962b49bf9a2275fcfd30de
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-11-15 21:11:27 -0800

    Update path for tensorflow activate

--- web/turk/run_prediction.sh
+++ web/turk/run_prediction.sh
@@ -1,4 +1,4 @@
-source ~/tensorflow/bin/activate
+source /home/ec2-user/tensorflow/bin/activate
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR/../../end_to_end/"
 echo "Input:" + "$@" 
