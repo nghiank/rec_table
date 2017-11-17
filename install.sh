8805b380b6cf2bb8e59c6f5231aac3dd3ab449e5
Nghia <nghia@Nghias-MacBook-Pro.local>
2017-11-16 18:43:31 -0800

    fix installation script

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 TENSORFLOW_PATH="/opt/python/current/app/tensorflow"
 if [ ! -d  $TENSORFLOW_PATH ]; then
     mkdir $TENSORFLOW_PATH
 fi
-virtualenv --system-site-packages -p python $TENSORFLOW_PATH 
+virtualenv --system-site-packages -p python3 $TENSORFLOW_PATH 
 source /opt/python/current/app/tensorflow/bin/activate
 pip3 install --upgrade tensorflow
 pip3 install Pillow
