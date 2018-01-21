b8b4e50bb12244311a113ccbc8b4f307f9f640ff
Nghia Nguyen <nghiank@hotmail.com>
2018-01-20 18:20:10 -0800

    Update extract_cell

--- extract/compile_run_sample.sh
+++ extract/compile_run_sample.sh
@@ -1,1 +1,1 @@
-make && ./extract_cell --inputFileName /Users/nghia/Downloads/a.jpg --outputFolder /Users/nghia/Desktop/tmp
+make && ./extract_cell --inputFileName /Users/nghia/Downloads/b.jpg --outputFolder /Users/nghia/Desktop/tmp
