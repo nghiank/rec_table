b243dd2432396cbb5ab113bf55e9ae16cc986a95
Nghia Nguyen <nghiank@hotmail.com>
2018-09-03 12:42:32 -0700

    adding import

--- web/turk/catalog/views/remove_image_api.py
+++ web/turk/catalog/views/remove_image_api.py
@@ -25,6 +25,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 @api_view(['POST'])
 @permission_classes((IsAuthenticated, ))
